c9cd7d4dafc721a05c39f5aa918b666acc809ffb
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-07-06 20:53:06 -0700

    feat: implement Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,36 @@
-export function Game({ children }) {
-  return <>{children}</>;
+import { useState } from "react";
+import { CardGroup, Card, Score } from "./index.jsx";
+
+export function Game({ items }) {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
+
+  const onIncreaseScore = () => setScore(score + 1);
+  const onRestartGame = () => setIsGameOver(false);
+  const onGameOver = () => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+    setIsGameOver(true);
+  };
+
+  return (
+    <>
+      <Score {...{ score, bestScore }} />
+      <CardGroup>
+        {items?.map((item) => (
+          <Card
+            {...{
+              item,
+              isGameOver,
+              endGame: onGameOver,
+              increaseScore: onIncreaseScore,
+              restartGame: onRestartGame,
+            }}
+          />
+        ))}
+      </CardGroup>
+    </>
+  );
 }
