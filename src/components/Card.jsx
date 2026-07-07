4d510ea57efe22b7b8a1753157f5cce75396582b
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-07-06 20:11:33 -0700

    feat: implement Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,21 @@
-export function Card() {}
+import { useState } from "react";
+
+export function Card({ img, altText, desc, endGame, increaseScore }) {
+  const [wasClicked, setWasClicked] = useState(false);
+
+  const handleClick = () => {
+    if (wasClicked) {
+      endGame();
+    } else {
+      increaseScore();
+      setWasClicked(true);
+    }
+  };
+
+  return (
+    <div className="card" onClick={handleClick}>
+      <img className="card-image" src={img} alt={altText} />
+      <p className="card-description">{desc}</p>
+    </div>
+  );
+}
