47dc5666d7067cd475e2bdcb866c9d43a39b7016
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-07-06 20:53:55 -0700

    feat: implement Score component

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,1 +1,8 @@
-export function Score() {}
+export function Score({ score, bestScore }) {
+  return (
+    <section aria-label="Scores" className="score-group">
+      <p className="score">Score: {score}</p>
+      <p className="best-score">Best: {bestScore}</p>
+    </section>
+  );
+}
