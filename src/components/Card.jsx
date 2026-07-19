6824ce383e5288700a1de8c554bf83251c7c3090
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-07-18 19:06:00 -0700

    feat(Card): animated card
    Each card have their own animation to provide feedback to the player
    for correct and wrong selection.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,40 @@ import { useState } from "react";
 
 export function Card({ img, altText = "", desc, onGameOver, onIncreaseScore }) {
   const [wasClicked, setWasClicked] = useState(false);
+  const [activeAnimation, setActiveAnimation] = useState("");
+
+  const onStaleClick = () => {
+    setActiveAnimation("stale");
+  };
+
+  const onFreshClick = () => {
+    setActiveAnimation("fresh");
+  };
+
+  const handleAnimationEnd = () => {
+    setActiveAnimation("");
+    if (activeAnimation === "stale") {
+      onGameOver();
+    } else if (activeAnimation === "fresh") {
+      onIncreaseScore();
+    }
+  };
 
   const handleClick = () => {
     if (wasClicked) {
-      onGameOver();
+      onStaleClick();
     } else {
-      onIncreaseScore();
+      onFreshClick();
       setWasClicked(true);
     }
   };
 
   return (
-    <button className="card" onClick={handleClick}>
+    <button
+      className={`card ${activeAnimation}`}
+      onClick={handleClick}
+      onAnimationEnd={handleAnimationEnd}
+    >
       <img className="card-image" src={img} alt={altText} />
       <h3 className="card-description">{desc}</h3>
     </button>
